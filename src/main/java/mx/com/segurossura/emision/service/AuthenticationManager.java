a13794038629b548013a4ac3958c81e1644f6696
cesar <cesar.cordero@segurossura.com.mx>
2017-05-19 11:51:54 -0500

    Renombrar action

--- src/main/java/mx/com/segurossura/emision/service/AuthenticationManager.java
+++ src/main/java/mx/com/segurossura/emision/service/AuthenticationManager.java
@@ -8,6 +8,12 @@ public interface AuthenticationManager {
 
 	UsuarioVO login(String user, String password) throws Exception;
 
+	/**
+	 * Obtiene los menus del usuario
+	 * @param usuario Objeto con datos del usuario
+	 * @return Menus del usuario en formato JSON
+	 * @throws Exception
+	 */
 	String menu(UsuarioVO usuario) throws Exception;
 
 	
