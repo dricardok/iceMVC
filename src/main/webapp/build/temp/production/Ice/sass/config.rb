9cfae516572d1b1dcdd98d7677e072c652998ff2
Ricardo Bautista Silva <ricardo.bautista@segurossura.com.mx>
2017-07-05 23:18:17 -0500

    Se suben archivos de webapp/build para despliegue en jBoss

---
+++ src/main/webapp/build/temp/production/Ice/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'E:\projects\eclipse_neon\iceMVC\src\main\webapp/build/.sass-cache'
---
+++ src/main/webapp/build/temp/production/Ice/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require E:\projects\eclipse_neon\iceMVC\src\main\webapp\app.js
