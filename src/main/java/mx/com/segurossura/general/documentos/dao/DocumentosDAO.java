49309f5c4fc3c6c7ddbbdae2b7349b0761523b3c
eortiz <Erick.Ortiz@segurossura.com.mx>
2017-07-19 20:59:40 -0500

    Se agregan cambios de consulta de documentos

--- src/main/java/mx/com/segurossura/general/documentos/dao/DocumentosDAO.java
+++ src/main/java/mx/com/segurossura/general/documentos/dao/DocumentosDAO.java
@@ -5,8 +5,25 @@ import java.util.List;
 import java.util.Map;
 
 public interface DocumentosDAO {
+    /**
+     * Se obtienen lista documentos de poliza
+     * 
+     * @param cdunieco
+     * @param cdramo
+     * @param estado
+     * @param nmpoliza
+     * @param nmsuplem
+     * @param ntramite
+     * @param cdsisrol
+     * @param dsdocume
+     * @param cdtipdoc
+     * @param start
+     * @param limit
+     * @return
+     * @throws Exception
+     */
     public List<Map<String, String>> obtenerDocumentos(String cdunieco, String cdramo, String estado, String nmpoliza, String nmsuplem, 
-            String ntramite, String cdsisrol, String dsdocume, long start, long limit) throws Exception;
+            String ntramite, String cdsisrol, String dsdocume, String cdtipdoc, long start, long limit) throws Exception;
     
     public void movimientoTdocupol() throws Exception;
     
