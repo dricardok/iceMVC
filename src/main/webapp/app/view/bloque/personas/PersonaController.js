7522b3acbe76c2e081aafdc26cc3be9a356632bb
cesar <cesar.cordero@segurossura.com.mx>
2017-08-07 20:04:20 -0500

    settimeout

--- src/main/webapp/app/view/bloque/personas/PersonaController.js
+++ src/main/webapp/app/view/bloque/personas/PersonaController.js
@@ -79,7 +79,7 @@ Ext.define('Ice.view.bloque.personas.PersonaController', {
     		Ice.log("Datos de persona a enviar: ",mpersona,tvaloper);
     		accion=view.getAccion();
 //    		view.fireEvent("personaGuardada", view, json.params.cdperson);
-    		Ext.Ajax.timeout = 9999999999999999;
+    		Ext.Ajax.setTimeout ( 9999999999999999);
     		Ice.request({
     			
     			url:Ice.url.bloque.personas.guardarPersona,
