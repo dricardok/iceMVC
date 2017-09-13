61c9a7ba9196da709627c555619eba661f666a25
jtezva <jtezva@gmail.com>
2017-09-12 19:52:56 -0500

    Se permiten 10 decimales

--- src/main/webapp/IceCore.js
+++ src/main/webapp/IceCore.js
@@ -1318,8 +1318,9 @@ var Ice = (
                     item.maxValue = config.maxvalue;
                 }
             }
-            if (item.xtype === 'numberfieldice') { // para todos los campos numericos si no hay definifo es 0
-                item.minValue = item.minValue || 0;
+            if (item.xtype === 'numberfieldice') {
+                item.minValue = item.minValue || 0; // para todos los campos numericos si no hay definifo es 0
+                item.decimalPrecision = 10; // se permiten 10 decimales
             }
             
             
