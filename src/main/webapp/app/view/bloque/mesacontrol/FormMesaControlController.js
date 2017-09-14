4df0b93b4f26c0e477876d26908e47ac127405e9
José Luna Perez <jose.luna@biosnettcs.com>
2017-09-13 21:59:18 -0500

    Correccion de paginador de mesa de control

--- src/main/webapp/app/view/bloque/mesacontrol/FormMesaControlController.js
+++ src/main/webapp/app/view/bloque/mesacontrol/FormMesaControlController.js
@@ -20,7 +20,21 @@ Ext.define('Ice.view.bloque.mesacontrol.FormMesaControlController', {
 			if(Ice.classic()) {
 				storegrid.removeAll();
 			}
-			storegrid.load({
+			
+			storegrid.getProxy().extraParams['params.cdunieco'] = formRefs.cdunieco.getValue();
+			storegrid.getProxy().extraParams['params.cdramo'] = formRefs.cdramo.getValue();
+			storegrid.getProxy().extraParams['params.estado'] = formRefs.estado.getValue();
+			storegrid.getProxy().extraParams['params.nmpoliza'] = formRefs.nmpoliza.getValue();
+			storegrid.getProxy().extraParams['params.cdagente'] = formRefs.cdagente.getValue();
+			storegrid.getProxy().extraParams['params.ntramite'] = formRefs.ntramite.getValue();
+			storegrid.getProxy().extraParams['params.estatus'] = formRefs.cdestadomc.getValue();
+			storegrid.getProxy().extraParams['params.fstatusi'] = formRefs.fstatusi.getValue();
+			storegrid.getProxy().extraParams['params.fstatusf'] = formRefs.fstatusf.getValue();
+			storegrid.getProxy().extraParams['params.nombre'] = formRefs.nombre.getValue();
+			storegrid.getProxy().extraParams['params.nmsolici'] = formRefs.nmsolici.getValue();
+			
+			
+			storegrid.loadPage(1,{
 				params: {
 					'params.cdunieco':formRefs.cdunieco.getValue(),
 					'params.cdramo':formRefs.cdramo.getValue(),
